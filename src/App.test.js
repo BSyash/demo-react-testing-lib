8b9b333446c313f765d29f0320c1a29442b932df
yash <yash@bigscal.com>
2022-10-10 18:35:55 +0530

    install packages.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/xyz/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("should first", () => {
+  const { debug,container,getByText } = render(<App />);
+  // const element  = screen.debug()
+  debug()
+  expect(getByText("xyz")).toBeInTheDocument("xyz");
+  
 });
